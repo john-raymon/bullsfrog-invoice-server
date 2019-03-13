77914fa22417bbb648ddf3e003c78dc994eb950d
John Ray <john@johnraymon.com>
2019-03-12 20:00:33 -0400

    adjust route action to post

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,25 @@
 var express = require('express');
+var { ViewBasedClient }  = require('knackhq-client');
 var router = express.Router();
 var auth = require('./auth')
 
 /* GET users listing. */
-router.get('/', auth.required, function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/login', function(req, res, next) {
+  console.log('THE APP ID IS !',process.env.APP_ID)
+  const client = new ViewBasedClient({app_id: process.env.APP_ID});
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (email && password) {
+    client.auth(email, password).then(response => {
+      res.json(response)
+    }).catch(error => {
+      res.status(500).json(error);
+    })
+  } else {
+    res.status(422).json('Please provide a username and password in the form of x-www-form-urlencoded')
+  }
 });
 
 
