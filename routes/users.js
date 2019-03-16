03aeb54f9c869795320a6c2beef0ed24ba939e30
John Ray <john@johnraymon.com>
2019-03-16 17:28:47 -0400

    route users/

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,6 @@ var auth = require('./auth')
 
 /* GET users listing. */
 router.post('/login', function(req, res, next) {
-  console.log('THE APP ID IS !',process.env.APP_ID)
   const client = new ViewBasedClient({app_id: process.env.APP_ID});
 
   const email = req.body.email;
@@ -18,7 +17,7 @@ router.post('/login', function(req, res, next) {
       res.status(500).json(error);
     })
   } else {
-    res.status(422).json('Please provide a username and password in the form of x-www-form-urlencoded')
+    res.status(422).json('Please provide a username and password')
   }
 });
 
