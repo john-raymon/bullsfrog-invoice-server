825b75f7db708258ead312899dbc3abf28489bc6
John Ray <john@johnraymon.com>
2019-03-22 15:58:10 -0400

    adjust GET /invoices-to-do endpoint to receive query parameters that translate to the ViewBasedClient arguments

--- routes/knack.js
+++ routes/knack.js
@@ -5,8 +5,9 @@ var auth = require('./auth')
 
 /* GET users listing. */
 router.get('/invoices-to-do', auth.required, function(req, res, next) {
+  console.log('the query is', req.query, req.query.page, req.query.rows_per_page)
   const client = new ViewBasedClient({app_id: process.env.APP_ID, token: req.knackAuth.token});
-  client.getAllRecords('445', '839').then((data) => {
+  client.getAllRecords('445', '839', req.query.filters, req.query.page, req.query.rows_per_page).then((data) => {
     if (!data.error) {
       return res.json(data.response.body)
     }
