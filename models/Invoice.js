a5591638580b905b4641b30d662d6d21f4478ba1
John Ray <john@johnraymon.com>
2019-03-29 21:48:20 -0400

    begin creating schema for Invoice model

--- models/Invoice.js
+++ models/Invoice.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose")
 
 const InvoiceSchema = new mongoose.Schema({
-  estimateName: { type: String , required: [ true, 'is required'] }
-  customer: {}
+  estimateName: { type: String , required: [ true, 'is required'] },
+  images: [String],
+  draft: { type: Boolean, default: true },
 })
