6363ef41402dc4ef2e6800a873b07a449b6e07e1
John Ray <john@johnraymon.com>
2019-03-30 02:04:14 -0400

    create schema for Room model

--- models/Invoice.js
+++ models/Invoice.js
@@ -4,4 +4,25 @@ const InvoiceSchema = new mongoose.Schema({
   estimateName: { type: String , required: [ true, 'is required'] },
   images: [String],
   draft: { type: Boolean, default: true },
+  invoiceTitle: String,
+  customer: {
+    customerKnackId: String,
+    address: String,
+    city: String,
+    state: String,
+    zipcode: String
+  },
+  salesTaxPercentageUsed: { type: Number, default: "0" },
+  claim: {
+    insuranceCarrier: String,
+    policyNumber: String,
+    claimNumber: String, // adjusters name replacement,
+    dateOfLoss: String,
+  },
+  rooms: [{ type: mongoose.Schema.Types.ObjectId, ref: "room" }],
+  totalCost: Number
 })
+
+const Invoice = mongoose.model('Invoice', InvoiceSchema)
+
+module.exports = Invoice
