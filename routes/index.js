e2809963eabbc63fbd07cba72c02a6714db7b0a0
John Ray <john@johnraymon.com>
2019-04-20 22:10:54 -0400

    add error handler to root level of api router

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,17 @@ router.use('/users', require('./users'));
 router.use('/knack', require('./knack'))
 
 // save invoices
-// router.use('/invoices', require('./invoices'))
+router.use('/invoices', require('./invoices'))
+
+
+// error handler; catches UnauthorizedError, otherwise calls next errorhandler in stack
+router.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).json({
+      error: { Authentication: "You must be authenticated as a host" }
+    });
+  }
+  next(err);
+});
 
 module.exports = router;
