f4e4385b7ae27362387fe2c6f5d6cc8915931052
John Ray <john@johnraymon.com>
2019-04-20 22:12:00 -0400

    require mongoose, findOrCreate, install findOrCreate plugin on Schema

--- models/Invoice.js
+++ models/Invoice.js
@@ -1,16 +1,17 @@
-var mongoose = require("mongoose")
+const mongoose = require("mongoose")
+const findOrCreate = require("mongoose-find-or-create")
 
 const InvoiceSchema = new mongoose.Schema({
-  estimateName: { type: String , required: [ true, 'is required'] },
+  id: { type: String, index: true },
+  invoiceName: { type: String },
   images: [String],
   draft: { type: Boolean, default: true },
-  invoiceTitle: String,
   customer: {
     customerKnackId: String,
+    fullName: String,
     address: String,
-    city: String,
-    state: String,
-    zipcode: String
+    cityState: String,
+    zipCode: String
   },
   salesTaxPercentageUsed: { type: Number, default: "0" },
   claim: {
@@ -19,9 +20,39 @@ const InvoiceSchema = new mongoose.Schema({
     claimNumber: String, // adjusters name replacement,
     dateOfLoss: String,
   },
-  rooms: [{ type: mongoose.Schema.Types.ObjectId, ref: "room" }],
-  totalCost: Number
-})
+  rooms: {
+    type: Map,
+    of: {
+      roomName: String,
+      lineItems: {
+        type: Map,
+        of: {
+          description: String,
+          laborCost: String,
+          materialCost: String,
+          quantity: String,
+          total: String,
+          totalLabor: String,
+          totalMaterial: String,
+          uom: String
+        }
+      },
+      roomTotals: {
+        totalCost: String,
+        totalLabor: String,
+        totalMaterial: String
+      },
+      length: String,
+      width: String,
+      height: String,
+    }
+  },
+  totalCost: String,
+  totalLaborCost: String,
+  totalMaterialCost: String
+}, { id: false})
+
+InvoiceSchema.plugin(findOrCreate)
 
 const Invoice = mongoose.model('Invoice', InvoiceSchema)
 
