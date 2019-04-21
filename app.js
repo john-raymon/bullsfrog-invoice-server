1afde3b4fa1356e802103e508c4e246bdbb3503f
John Ray <john@johnraymon.com>
2019-04-20 22:09:34 -0400

    add errorhandlers at root level and also at routes level, remove import of deleted rooms model,

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+const mongoose = require("mongoose")
 
 var app = express();
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+if (isProduction) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect("mongodb://localhost/bullfrog_invoices", { useNewUrlParser: true });
+  mongoose.set("debug", true);
+}
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,12 +24,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // require models
-require("./models/Room")
 require("./models/Invoice")
 
 app.use(require('./routes'));
 
-if (process.env.NODE_ENV === 'production') {
+if (isProduction) {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -28,5 +37,26 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
+
+// forward 404s to error handler
+app.use(function(req, res, next) {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  if (isProduction) {
+    console.log(err.stack); // print stacktrace
+  }
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message || "",
+      error: err
+    }
+  });
+});
 
 module.exports = app;
