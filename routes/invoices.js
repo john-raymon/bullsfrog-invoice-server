d454be5adbd2083e6ac2c6da3da0b11731814b31
John Ray <john@johnraymon.com>
2019-04-23 05:12:22 -0400

    add to routes/invoices.js : adjust updating of rooms to be able to identify removed rooms in the request allowing us to unset those rooms in the document. also implemented endpoint to mark invoice as not a draft, while also generating a url and saving it knack connected knack customer

--- routes/invoices.js
+++ routes/invoices.js
@@ -1,11 +1,51 @@
 const express = require('express')
 const router = express.Router()
+const request = require('request')
 const cloudinary = require("cloudinary")
 const auth = require('./auth')
 const parser = require('./../config/multer-cloudinary')
 
 // Models
 const Invoice = require('./../models/Invoice')
+
+
+router.post('/:invoiceUUID/generate-invoice', auth.required, function(req, res, next) {
+  const { invoiceUUID } = req.params
+  return Invoice.findOne({ id: invoiceUUID }).then((invoice) => {
+    invoice.draft = false
+    return invoice.save()
+  }).then(({ customer, id:invoiceUUID }) => {
+    const { customerKnackId } = customer
+    if (customerKnackId.trim()) {
+      const generatedURL = `http://localhost:3000/invoices/pdf/${invoiceUUID}`
+      // save this url on Knack database with a connection to customer with customerKnackId
+      request({
+        url: "https://api.knack.com/v1/objects/object_78/records",
+        method: "POST",
+        headers: {
+          "X-Knack-REST-API-Key": process.env.KNACK_API_KEY,
+          "X-Knack-Application-Id": process.env.APP_ID
+        },
+        form: {
+        	field_574: {
+            url : generatedURL
+          },
+        	field_573: customerKnackId
+        },
+        json: true
+      },
+      function(err, response, body) {
+        if (err) {
+          return res.status(400).json({ error: err });
+        }
+        return res.json(body);
+      });
+    } else {
+      return res.json({ success: true })
+    }
+  })
+  .catch(next)
+})
 
 // Destroy image from existing Invoice document, and also from Cloudinary folder
 router.post("/:invoiceUUID/remove-image/:publicId", auth.required, function(req, res, next) {
@@ -58,6 +98,9 @@ router.post('/:invoiceUUID', auth.required, parser.array('invoiceImages'), funct
       if (req.body.invoiceName !== undefined || typeof req.body.invoiceName !== 'undefined') {
         invoice.invoiceName = req.body.invoiceName
       }
+      if (req.body.customerKnackId !== undefined || typeof req.body.customerKnackId !== 'undefined') {
+        invoice.customer.customerKnackId = req.body.customerKnackId
+      }
       if (req.body.customersFullName !== undefined || typeof req.body.customersFullName !== 'undefined') {
         invoice.customer.fullName = req.body.customersFullName
       }
@@ -84,10 +127,18 @@ router.post('/:invoiceUUID', auth.required, parser.array('invoiceImages'), funct
       }
       if (req.body.rooms !== undefined || typeof req.body.rooms !== 'undefined') {
         const rooms = req.body.rooms
-        const roomUUIDs = Object.keys(rooms)
-        if (roomUUIDs.length > 0) {
-          invoice.rooms = rooms
+        const existingRoomsObj = {
         }
+        for (let [key, value] of (invoice.rooms || new Map())) {
+          existingRoomsObj[key] = value
+        }
+        console.log('these are the existing keys', existingRoomsObj)
+        const allRooms = Object.keys(existingRoomsObj).reduce((roomObj, roomUUID) => {
+          roomObj[roomUUID] = rooms[roomUUID]
+          return roomObj
+        },{})
+        console.log('these are the rooms', allRooms)
+        invoice.rooms = {...existingRoomsObj, ...allRooms, ...rooms}
       }
       if (req.body.totalCost !== undefined || typeof req.body.totalCost !== 'undefined') {
         invoice.totalCost = req.body.totalCost
